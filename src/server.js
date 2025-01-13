380edd27929d875469f9de9c00440525c763294f
Midhun U <midhunvkm689@gmail.com>
2025-01-13 18:31:07 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app )
 //middlewares
 app.use(express.json({limit : "10mb"}))
 app.use(express.urlencoded({extended : true}))
-app.use(cors({origin : "https://chatify-gray.vercel.app" , credentials : true}))
+app.use(cors({origin : "https://chatify-gray.vercel.app/" , credentials : true}))
 app.use(cookieParser())
 
 //route middleware for authentication
